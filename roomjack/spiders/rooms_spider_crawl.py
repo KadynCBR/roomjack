10f195e8ecffb5a11caf35054d37f2c293afed30
KadynCBR <kadyn_martinez@hotmail.com>
2017-02-11 18:40:50 -0800

    added .json to gitignore, finished room example.

--- roomjack/spiders/rooms_spider_crawl.py
+++ roomjack/spiders/rooms_spider_crawl.py
@@ -9,7 +9,7 @@
 #------------------------------------------------------------------------------
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import linkextractors
+from scrapy.linkextractors import LinkExtractor
 
 # this is the actual spider
 class RoomSpider(CrawlSpider):
@@ -17,9 +17,53 @@ class RoomSpider(CrawlSpider):
     name = "roomcrawl"
     # Since you're automating traversal, set an allowable domain so
     # your crawspider doesn't feel the need to break off the website.
+    allowed_domains = ["sfbay.craigslist.org"]
+    # Starting points for your spider to crawl from.
     start_urls = (
-    "sfbay.craigslist.org",
+    "https://sfbay.craigslist.org/search/apa",
     )
+
+    # Define rules for the spider to /crawl/
+    rules = (Rule(LinkExtractor(allow=(),
+                                # The link extractor can only follow links based
+                                # on these xpath expresions.
+                                restrict_xpaths=('//a[@class="button next"]',)),
+                  # the callback for the pages the spider crawls.
+                  callback="parse_page",
+                  # follow links?
+                  follow=True),
+            )
+
     # This is the function that will run on every url response in the
-    # the spider crawls
+    # the crawlSpider's 'path'.
     def parse_page(self, response):
+        # define the data you're searching for in xpath expression
+        room_listings = response.xpath('//li[@class="result-row"]')
+        # This variable now contains all the listings on the page response.
+        # Note: it just's still html, just now every element starts at the
+        # beginning of each "result-row" root.
+
+        # iterate through rooms in the listings, again, room contains
+        # a block of html.
+        for room in room_listings:
+            # here we grab the data we want from the room html.
+
+            # NOTE: extract() returns a list, so you'll want to specify the
+            #       element you want.
+            #       otherwise use extract_first
+            listing_name = room.xpath('p/a/text()').extract()[0]
+            listing_price = room.xpath(
+            'p/span[2]/span[1]'
+            ).extract()
+            listing_date = room.xpath('//p/time/@datetime').extract_first()
+            # This appends the start url
+            listing_detail_link = self.allowed_domains[0] +\
+                                  room.xpath('//p/a/@href').extract_first()
+            # At this point you've gotten every room. You can do what you want
+            # with the data, or yield it so you can easily output it to json.
+            yield {
+                'name' : listing_name,
+                'price': listing_price,
+                'date' : listing_date,
+                'link' : listing_detail_link
+            }
